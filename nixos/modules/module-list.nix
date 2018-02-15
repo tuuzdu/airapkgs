2c2d9c6a40f7094500ae2a2a72f86ae0a971b76a
Alexander Krupenkin <mail@akru.me>
2018-02-15 11:51:56 +0300

    nixos/parity: enable in module list

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -514,6 +514,7 @@
   ./services/networking/openntpd.nix
   ./services/networking/openvpn.nix
   ./services/networking/ostinato.nix
+  ./services/networking/parity.nix
   ./services/networking/pdnsd.nix
   ./services/networking/polipo.nix
   ./services/networking/powerdns.nix

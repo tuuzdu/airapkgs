e4a930c5254c28c6dbefabd0dab4636a537a882d
Alexander Krupenkin <mail@akru.me>
2018-03-10 18:06:19 +0300

    freeopcua: 2016-04-18 -> 2018-01-26

--- pkgs/development/libraries/freeopcua/default.nix
+++ pkgs/development/libraries/freeopcua/default.nix
@@ -4,22 +4,25 @@
 , pkgconfig
 , boost
 , libxml2
+, python3
 }:
 
 stdenv.mkDerivation {
-  name = "freeopcua_2016-04-18";
+  name = "freeopcua_2018-01-26";
 
   src = fetchFromGitHub {
     owner = "FreeOpcUa";
     repo = "freeopcua";
-    rev = "43b86aea4d33c7f3fb982b95e4248d3992c10961";
-    sha256 = "0njr7p7j55w48axzfxcbr14zc0g8h35dmq27rc3kxlc438xdj0b8";
+    rev = "18e5a08531ab19f8c4f00dad1bee3d0597ee882c";
+    sha256 = "0d7884mj7ar324rdhs801a62lz1gcl50wk8inb96w6h7p26xvc8z";
   };
 
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ boost libxml2 ];
+  buildInputs = [ boost libxml2 python3 ];
 
-  cmakeFlags = [ "-DBUILD_SERVER=OFF" ];
+  cmakeFlags = [ "-DBUILD_SERVER=OFF" "-DSSL_SUPPORT_MBEDTLS=OFF" ];
+
+  patches = [ ./pkgconfig_generator.patch ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/FreeOpcUa/freeopcua;

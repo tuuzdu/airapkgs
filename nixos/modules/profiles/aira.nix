d9f2968a5ca306bd758985a2423bae23ac32beef
Alexander Krupenkin <mail@akru.me>
2018-04-11 00:42:28 +0300

    nixos: added aira profile

--- nixos/modules/profiles/aira.nix
+++ nixos/modules/profiles/aira.nix
@@ -1,10 +1,16 @@
-{ config, lib, pkgs, ... }:
-
-with lib;
+{ config, ... }:
 
 {
 
-  nix.binaryCaches = [ https://cache.nixos.org https://hydra.aira.life ];
-  nix.binaryCachePublicKeys = [ "hydra.aira.life-1:StgkxSYBh18tccd4KUVmxHQZEUF7ad8m10Iw4jNt5ak=" ];
+  nix = {
+    binaryCaches = [
+      https://cache.nixos.org
+      https://hydra.aira.life
+    ];
+
+    binaryCachePublicKeys = [
+      "hydra.aira.life-1:StgkxSYBh18tccd4KUVmxHQZEUF7ad8m10Iw4jNt5ak="
+    ];
+  };
 
 }

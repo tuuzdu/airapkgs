fbf6179151964155be6e32803aa4d0d47ad642cb
Alexander Krupenkin <mail@akru.me>
2018-04-12 01:42:45 +0300

    nixos/aira: enable cjdns service

--- nixos/modules/profiles/aira.nix
+++ nixos/modules/profiles/aira.nix
@@ -13,4 +13,26 @@
     ];
   };
 
+  services.cjdns = {
+    enable = true;
+
+    ETHInterface.bind = "all";
+    UDPInterface.bind = "0.0.0.0:42000";
+
+    # AIRA project cjdns node
+    UDPInterface.connectTo."52.232.72.83:31259" = {
+      password = "tt3yb4613wgh3sgfsgkg1fvk24k6hnk";
+      publicKey = "jyl980gs5513dw5x19qp3khb6337ljsx3sgwbsmnsvvyb5jdcw90.k";
+    };
+
+    # akru personal cjdns node
+    UDPInterface.connectTo."164.132.111.49:53741" = {
+      password = "cr36pn2tp8u91s672pw2uu61u54ryu8";
+      publicKey = "35mdjzlxmsnuhc30ny4rhjyu5r1wdvhb09dctd1q5dcbq6r40qs0.k";
+    };
+  };
+
+  # Open cjdns daemon port
+  networking.firewall.allowedUDPPorts = [ 42000 ];
+
 }

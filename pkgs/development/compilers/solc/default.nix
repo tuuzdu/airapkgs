03bbdf0cd7dbec1441c9ff55ee101da4545a8bbe
Alexander Krupenkin <mail@akru.me>
2018-04-18 15:04:02 +0300

    solc: 0.4.21 -> 0.4.22

--- pkgs/development/compilers/solc/default.nix
+++ pkgs/development/compilers/solc/default.nix
@@ -1,7 +1,7 @@
 { stdenv, fetchzip, fetchurl, boost, cmake, z3 }:
 
 let
-  version = "0.4.21";
+  version = "0.4.22";
   jsoncppURL = https://github.com/open-source-parsers/jsoncpp/archive/1.7.7.tar.gz;
   jsoncpp = fetchzip {
     url = jsoncppURL;
@@ -15,7 +15,7 @@ stdenv.mkDerivation {
   # Cannot use `fetchFromGitHub' because of submodules
   src = fetchurl {
     url = "https://github.com/ethereum/solidity/releases/download/v${version}/solidity_${version}.tar.gz";
-    sha256 = "1c0712dc298dz4jixyapr2s9mwv0shh1bkalvmq4d24bcrx1dgzz";
+    sha256 = "1fvhn3ca8l8kx0lcp8mdxmvgfxi5g4223hysgh3wy49nvchj0wxm";
   };
 
   patchPhase = ''

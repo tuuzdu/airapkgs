223e8d857bdfcf5dd9fdb5dc59e5ab9aee9bd264
Alexander Krupenkin <mail@akru.me>
2018-05-02 00:00:02 +0300

    pythonPackages.eth-abi: v1.0.0-beta.0 -> v1.0.0

--- pkgs/development/python-modules/eth-abi/default.nix
+++ pkgs/development/python-modules/eth-abi/default.nix
@@ -6,7 +6,7 @@
 
 let
   pname = "eth-abi";
-  version = "v1.0.0-beta.0";
+  version = "v1.0.0";
 in buildPythonPackage rec {
   name = "${pname}-${version}";
 
@@ -14,7 +14,7 @@ in buildPythonPackage rec {
     owner = "ethereum";
     repo = "${pname}";
     rev = "${version}";
-    sha256 = "0cck9lxll0i19qnd8d51d3n6q647h76q6q1s2vbjq6a3v65v2cxf";
+    sha256 = "04jiwri35yz0sfnbpldc8ll484mxxlwraifi345nxp8g48w12c6g";
   };
 
   propagatedBuildInputs = [ eth-utils ];

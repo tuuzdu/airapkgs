1accf8441bf34a02d4f3fab54dfdc67868118264
Alexander Krupenkin <mail@akru.me>
2018-05-12 14:23:24 +0300

    parity: 1.9.7 -> 1.10.3

--- pkgs/applications/altcoins/parity/default.nix
+++ pkgs/applications/altcoins/parity/default.nix
@@ -1,7 +1,7 @@
 let
-  version     = "1.9.7";
-  sha256      = "1h9rmyqkdv2v83g12dadgqflq1n1qqgd5hrpy20ajha0qpbiv3ph";
-  cargoSha256 = "0ss5jw43850r8l34prai5vk1zd5d5fjyg4rcav1asbq6v683bww0";
-  patches     = [ ./patches/vendored-sources-1.9.patch ];
+  version     = "1.10.3";
+  sha256      = "1rci8rdsc05fsfr6man0k9qrmymss77x9igyr928x8d7b6jvqa3s";
+  cargoSha256 = "1sxqkg0wajz1k456kxw3401vcf6fzm3r2zp6ny8vwl2jwmz33s5k";
+  patches     = [ ./patches/vendored-sources-1.10.patch ];
 in
   import ./parity.nix { inherit version sha256 cargoSha256 patches; }

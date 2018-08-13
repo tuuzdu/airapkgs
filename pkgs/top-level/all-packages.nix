20a594082b1304144193486eb804340f0998efa1
tuuzdu <tuuzdu@mail.ru>
2018-08-13 23:35:42 +0300

    tf: init at 1.12.0. Fix dji_sdk build.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23443,6 +23443,8 @@ in
 
   uuid_msgs = callPackage ../development/ros-modules/uuid_msgs { };
 
+  tf = callPackage ../development/ros-modules/tf { };
+
   tf2 = callPackage ../development/ros-modules/tf2 { };
 
   tf2_py = callPackage ../development/ros-modules/tf2_py { };

b50cec2a9a594862e046a3a9f7c2a8f77db21128
strdn <eilyichev@aira.life>
2018-09-20 16:44:01 +0300

    revert hash changes for aarch64 bootstrap-tools

--- pkgs/stdenv/linux/bootstrap-files/aarch64.nix
+++ pkgs/stdenv/linux/bootstrap-files/aarch64.nix
@@ -6,6 +6,6 @@
   };
   bootstrapTools = import <nix/fetchurl.nix> {
     url = http://nixos-arm.dezgeg.me/bootstrap-aarch64-2017-03-11-bb3ef8/bootstrap-tools.tar.xz;
-    sha256 = "0gc59si8hdgz8bysh3h8rpn5r5p4m6qfxmivii469z3l4q3xjxab";
+    sha256 = "1075d5n4yclbhgisi6ba50601mw3fhivlkjs462qlnq8hh0xc7nq";
   };
 }

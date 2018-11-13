97abfe2ac735e76fd545ce7885bb97ec0cd116a3
Alexander Krupenkin <mail@akru.me>
2018-11-13 17:04:11 +0300

    aira: switch to robonomics gen 3

--- nixos/modules/installer/aira.nix
+++ nixos/modules/installer/aira.nix
@@ -13,7 +13,7 @@
   services.liability.enable = true;
   services.liability.web3_http_provider = "https://mainnet.infura.io/v3/cd7368514cbd4135b06e2c5581a4fff7";
   services.liability.web3_ws_provider = "wss://mainnet.infura.io/ws";
-  services.liability.lighthouse = "airalab.lighthouse.2.robonomics.eth";
+  services.liability.lighthouse = "airalab.lighthouse.3.robonomics.eth";
 
   # Enable graph monitoring
   services.aira-graph.enable = true;
@@ -28,7 +28,7 @@
     syntaxHighlighting.enable = true;
     loginShellInit = ''
       echo -e "Starting..."
-      sleep 7
+      sleep 5
       ${pkgs.figlet}/bin/figlet AIRA
       export ADDRESS=`cat /var/lib/liability/keyfile|${pkgs.jq}/bin/jq ".address"`
       echo -e "\nMy Ethereum address is $ADDRESS"

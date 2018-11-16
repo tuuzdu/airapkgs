453e06e09d264799b8eb84549410acea2b5626a0
Alexander Krupenkin <mail@akru.me>
2018-11-16 12:54:16 +0300

    dji_sdk: 3.6.0 -> 3.7.0

--- pkgs/development/ros-modules/dji_sdk/default.nix
+++ pkgs/development/ros-modules/dji_sdk/default.nix
@@ -1,40 +1,36 @@
 { stdenv
 , mkRosPackage
 , fetchFromGitHub
-, catkin
 , djiosdk
 , nav_msgs
 , roscpp
 , rospy
 , sensor_msgs
-, glibcLocales
 , tf
 }:
 
-let
-  pname = "dji_sdk";
-  version = "3.6.0";
-
-in mkRosPackage {
+mkRosPackage rec {
   name = "${pname}-${version}";
+  pname = "dji_sdk";
+  version = "3.7.0";
 
   src = fetchFromGitHub {
     owner = "dji-sdk";
     repo = "Onboard-SDK-ROS";
-    rev = "${version}";
-    sha256 = "08icpaja0kk231l8w0np4wxi2rlk4jbf91sm0ybqsddcyw6akka7";
+    rev = version;
+    sha256 = "1ikci92clia90ysy2g234gdwhc0jwhls1cwnh94jghsaxincxm4x";
   };
 
-  patches = [ ./catkin_python_setup.patch ];
-  
-  propagatedBuildInputs = [ tf glibcLocales catkin djiosdk nav_msgs roscpp rospy sensor_msgs ];
-
-  preConfigure = ''
-    export LC_ALL="en_US.UTF-8"
+  patches = [ ./non-ascii-genmsg-fix.patch ];
+  postPatch = ''
     rm -rf dji_sdk_demo
     mv dji_sdk/* .
     rmdir dji_sdk
+    sed -i '/catkin_python_setup()/d' CMakeLists.txt
   '';
+
+  buildInputs = [ djiosdk ];
+  propagatedBuildInputs = [ tf nav_msgs roscpp rospy sensor_msgs ];
 
   meta = with stdenv.lib; {
     description = "Official ROS packages for DJI onboard SDK.";

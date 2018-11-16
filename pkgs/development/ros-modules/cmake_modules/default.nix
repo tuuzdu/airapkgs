e69b4578b1956f8a7eb821b438ad33837087984e
Alexander Krupenkin <mail@akru.me>
2018-11-16 12:03:17 +0300

    mavros: 0.26.1 -> 0.27.0

--- pkgs/development/ros-modules/cmake_modules/default.nix
+++ pkgs/development/ros-modules/cmake_modules/default.nix
@@ -1,24 +1,20 @@
 { stdenv
 , mkRosPackage
 , fetchFromGitHub
-, catkin
 }:
 
-let
+mkRosPackage rec {
+  name = "${pname}-${version}";
   pname = "cmake_modules";
   version = "0.4.1";
-
-in mkRosPackage {
-  name = "${pname}-${version}";
+  rosdistro = "melodic";
 
   src = fetchFromGitHub {
     owner = "ros-gbp";
     repo = "${pname}-release";
-    rev = "release/lunar/${pname}/${version}-0";
+    rev = "release/${rosdistro}/${pname}/${version}-0";
     sha256 = "1z85gmadn5llnzabkkij2l5xf39mxc48v8i4m08vnpnljzr06x0i";
   };
-
-  propagatedBuildInputs = [ catkin ];
 
   meta = with stdenv.lib; {
     description = "A common repository for CMake Modules for ROS";

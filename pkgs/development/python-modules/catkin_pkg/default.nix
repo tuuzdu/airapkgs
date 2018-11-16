810c5212859f12a76f6eea5b61f905d5256e9b69
Alexander Krupenkin <mail@akru.me>
2018-11-16 15:08:34 +0300

    catkin_pkg: 0.4.1 -> 0.4.9

--- pkgs/development/python-modules/catkin_pkg/default.nix
+++ pkgs/development/python-modules/catkin_pkg/default.nix
@@ -8,16 +8,12 @@
 
 let
   pname = "catkin_pkg";
-  version = "0.4.1";
-  sha256 = "0wm9ali5329ci0jhlqcfcn7i859ri0h3ly752006mam7jjggdgma";
+  version = "0.4.9";
+  sha256 = "1vz3c6ri71wv09vac2dafr79w3107k7pq00fp5w9ci7vqpmx7l5f";
 
 in buildPythonPackage rec { 
   name = "${pname}-${version}";
   src = fetchPypi { inherit pname version sha256; };
-
-  prePatch = ''
-    sed -i '/argparse/d' setup.py
-  '';
 
   propagatedBuildInputs = [ pyparsing docutils dateutil ];
 

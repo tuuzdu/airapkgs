a895ca72e0c67b383d802696508d7e3f67366bb0
Alexander Krupenkin <mail@akru.me>
2018-11-16 13:04:35 +0300

    nixos/release-aira: temporary disable aarch64 builds

--- nixos/release-aira.nix
+++ nixos/release-aira.nix
@@ -4,7 +4,7 @@
 
 { nixpkgs ? { outPath = (import ../lib).cleanSource ./..; revCount = 56789; shortRev = "gfedcba"; }
 , stableBranch ? false
-, supportedSystems ? [ "x86_64-linux" "aarch64-linux" ] # no i686-linux
+, supportedSystems ? [ "x86_64-linux" ] # no i686-linux
 }:
 
 let

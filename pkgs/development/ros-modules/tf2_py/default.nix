59f6a39dd4e2ba68c1caf85565f8042b112be05a
Alexander Krupenkin <mail@akru.me>
2018-11-16 12:02:43 +0300

    tf2: 0.5.18 -> 0.6.4

--- pkgs/development/ros-modules/tf2_py/default.nix
+++ pkgs/development/ros-modules/tf2_py/default.nix
@@ -1,27 +1,24 @@
 { stdenv
 , mkRosPackage
 , fetchFromGitHub
-, catkin
 , rospy
 , tf2
 }:
 
-let
-  pname = "tf2_py";
-  version = "0.5.18";
-  rosdistro = "melodic";
-
-in mkRosPackage {
+mkRosPackage rec {
   name = "${pname}-${version}";
+  pname = "tf2_py";
+  version = "0.6.4";
+  rosdistro = "melodic";
 
   src = fetchFromGitHub {
     owner = "ros-gbp";
     repo = "geometry2-release";
     rev = "release/${rosdistro}/${pname}/${version}-0";
-    sha256 = "0p01sjirj43vdznv4mh4lzz9nq836qwvnqw9mr1pbajsp4wg7gkf";
+    sha256 = "1jm3mxm3lvil4wijx3r34d4jxryxihbg4y90i2fnd1x5d418dprj";
   };
 
-  propagatedBuildInputs = [ catkin tf2 rospy ];
+  propagatedBuildInputs = [ tf2 rospy ];
 
   meta = with stdenv.lib; {
     description = "Binds the tf2 functions and exceptions to python.";
--- pkgs/development/ros-modules/tf2_ros/default.nix
+++ pkgs/development/ros-modules/tf2_ros/default.nix
@@ -1,28 +1,25 @@
 { stdenv
 , mkRosPackage
 , fetchFromGitHub
-, catkin
 , actionlib
 , message_filters
 , tf2_py
 }:
 
-let
-  pname = "tf2_ros";
-  version = "0.5.18";
-  rosdistro = "melodic";
-
-in mkRosPackage {
+mkRosPackage rec {
   name = "${pname}-${version}";
+  pname = "tf2_ros";
+  version = "0.6.4";
+  rosdistro = "melodic";
 
   src = fetchFromGitHub {
     owner = "ros-gbp";
     repo = "geometry2-release";
     rev = "release/${rosdistro}/${pname}/${version}-0";
-    sha256 = "0hgwnin3bg914f8jimg5bnzxswcl8qw2jv8cp4h2iij3ampjdzha";
+    sha256 = "0yr069jpffzvajqyj509jjfpd9f7zb3glqsyhxbyjhaqknwhby4c";
   };
 
-  propagatedBuildInputs = [ catkin actionlib message_filters tf2_py ];
+  propagatedBuildInputs = [ actionlib message_filters tf2_py ];
 
   meta = with stdenv.lib; {
     description = "ROS bindings for the tf2 library, for both Python and C++.";

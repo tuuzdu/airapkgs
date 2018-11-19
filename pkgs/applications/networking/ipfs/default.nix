1cc96e8a6c8f78c4f68992bb5e273f7a6a92f339
Alexander Krupenkin <mail@akru.me>
2018-11-19 17:33:43 +0300

    ipfs: 0.4.17 -> 0.4.18

--- pkgs/applications/networking/ipfs/default.nix
+++ pkgs/applications/networking/ipfs/default.nix
@@ -1,16 +1,24 @@
-{ stdenv, buildGoPackage, fetchFromGitHub, fetchgx }:
+{ stdenv, buildGo111Package, go_1_11, gx, gx-go, fetchFromGitHub, fetchgx }:
 
-buildGoPackage rec {
+let
+  fetchgx' = fetchgx.override {
+    go = go_1_11;
+    gx = gx.override { buildGoPackage = buildGo111Package; };
+    gx-go = gx-go.override { buildGoPackage = buildGo111Package; }; 
+  };
+  buildGoPackage = buildGo111Package; 
+
+in buildGoPackage rec {
   name = "ipfs-${version}";
-  version = "0.4.17";
+  version = "0.4.18";
   rev = "v${version}";
 
   goPackagePath = "github.com/ipfs/go-ipfs";
 
   extraSrcPaths = [
-    (fetchgx {
+    (fetchgx' {
       inherit name src;
-      sha256 = "0grdgnr67r3qh0ppc3flrhcw8zlvx10mxypd8q2mhkil9w4dpcna";
+      sha256 = "05d5m6c2i2kl4rvb0hddyqbidn76ljr2zryi8v2r9i8dbi0164gm";
     })
   ];
 
@@ -18,7 +26,7 @@ buildGoPackage rec {
     owner = "ipfs";
     repo = "go-ipfs";
     inherit rev;
-    sha256 = "18skmchdqd54wfqhibscqvc360l5ig6vmxd73ivf3bcpj3zvgq7q";
+    sha256 = "0h4j18qpycfmmlhb9khvhbk8c1zqajflvw8gk3l8j7wxrxh5j2s6";
   };
 
   meta = with stdenv.lib; {

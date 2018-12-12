2d9bae1493a080ecae35903691b0b6be25bc68ce
Alexander Krupenkin <mail@akru.me>
2018-12-12 12:39:43 +0300

    robonomics_comm-nightly: updated

--- pkgs/applications/science/robotics/aira/robonomics_comm/nightly.nix
+++ pkgs/applications/science/robotics/aira/robonomics_comm/nightly.nix
@@ -13,8 +13,8 @@ mkRosPackage rec {
   src = fetchFromGitHub {
     owner = "airalab";
     repo = "robonomics_comm";
-    rev = "7279b38f02152b4707cda2732d3afc462d65a3a6";
-    sha256 = "1gxfm4lxk9sbx0xsai03szkf4mj31gnhn08mrf2pp88p4khxz51b";
+    rev = "a1e0d7ed178119d4b276a4ca4df6f29624aff527";
+    sha256 = "1cv5xy1fiwpxkply1l8p94aqip6r8jdg7x772zqy0q5c8pzrjls2";
   };
 
   propagatedBuildInputs = with python3Packages;

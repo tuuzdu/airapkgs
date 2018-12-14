92cd7d753c0e9557fe166fa838633f2d906f9f32
Alexander Krupenkin <mail@akru.me>
2018-12-14 16:51:49 +0300

    pythonPackages.cytoolz: fix build

--- pkgs/development/python-modules/cytoolz/default.nix
+++ pkgs/development/python-modules/cytoolz/default.nix
@@ -17,15 +17,6 @@ buildPythonPackage rec {
     sha256 = "84cc06fa40aa310f2df79dd440fc5f84c3e20f01f9f7783fc9c38d0a11ba00e5";
   };
 
-  patches = [
-    # temporal fix for a test failure: https://github.com/pytoolz/cytoolz/issues/122
-    (fetchpatch {
-      name = "py37.patch";
-      url = https://salsa.debian.org/python-team/modules/python-cytoolz/raw/5ce4158deefc47475d1e76813f900e6c72ddcc6e/debian/patches/py37.patch;
-      sha256 = "1z29y7s5n751q3f74r3bz0f48yg6izvi68hc4pkwcalxmkq5r1n9";
-    })
-  ];
-
   # Extension types
   disabled = isPyPy;
 

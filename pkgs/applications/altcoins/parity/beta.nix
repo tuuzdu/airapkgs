7c8dde5003f2f0326ec7492e6ad674360a369998
Alexander Krupenkin <mail@akru.me>
2018-12-14 03:31:42 +0300

    parity: fixed deps hashes

--- pkgs/applications/altcoins/parity/beta.nix
+++ pkgs/applications/altcoins/parity/beta.nix
@@ -1,6 +1,6 @@
 let
   version     = "2.2.4";
   sha256      = "12qcfmc56vnay25nlflgwhm3iwlr7hd286wzzanlsalizaj5s5ja";
-  cargoSha256 = "11cwzqd459ld0apl2wnarfc4nb6j9j0dh26y3smvr0zsxvaz1r53";
+  cargoSha256 = "0pv4j111f1wlnqgqqx2q9z0ns529cc1c35sr9mz8jxywya7v6im5";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }
--- pkgs/applications/altcoins/parity/default.nix
+++ pkgs/applications/altcoins/parity/default.nix
@@ -1,6 +1,6 @@
 let
   version     = "2.1.9";
   sha256      = "1xxpv2cxfcjwxfxkn2732y1wxh9rpiwmlb2ij09cg5nph669hy0v";
-  cargoSha256 = "1v44l90bacw8d3ilnmrc49dxdpyckh7iamylkpa1pc0rrpiv5vy4";
+  cargoSha256 = "12fv5g6ji7b2r62gr7am5vpsh3jp8jh74q738hkjxm268gcrk9yz";
 in
   import ./parity.nix { inherit version sha256 cargoSha256; }

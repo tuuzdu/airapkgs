13b10e0b06ffa41b57e293d08d9eed9129b5f94a
Alexander Krupenkin <mail@akru.me>
2018-12-14 10:17:35 +0300

    robonomics-tools: 0.1.0.0 -> 0.2.0.0

--- pkgs/applications/science/robotics/aira/robonomics-tools.nix
+++ pkgs/applications/science/robotics/aira/robonomics-tools.nix
@@ -7,13 +7,13 @@
 
 mkDerivation rec {
   pname = "robonomics-tools";
-  version = "0.1.0.0";
+  version = "0.2.0.0";
 
   src = fetchFromGitHub {
     owner = "airalab";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0d3wmzfx86bhqgzbmw70aqi17gdnyb686r669nmfqqi2lap27n5f";
+    sha256 = "1s3wcq8ib1miabddasq12cpxzxm5sxwgrn2vkj0c67cyfs1ksiz7";
   };
 
   isLibrary = true;

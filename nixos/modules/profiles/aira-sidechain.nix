60276580f1a805d7a00f03628f838d3c26d0a31c
Alexander Krupenkin <mail@akru.me>
2018-12-20 16:53:20 +0300

    profiles/aira-sidechain: added ENS address

--- nixos/modules/profiles/aira-sidechain.nix
+++ nixos/modules/profiles/aira-sidechain.nix
@@ -7,5 +7,6 @@
     web3_http_provider = "https://sidechain.aira.life";
     web3_ws_provider = "wss://sidechain.aira.life/ws";
     lighthouse = "airalab.lighthouse.4.robonomics.sid";
+    ens = "0x2286921A25b3f2Dfe4E97B73E88EeB5f3E48FFd7";
   };
 }

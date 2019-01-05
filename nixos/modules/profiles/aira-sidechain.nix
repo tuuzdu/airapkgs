56139bee44a5d93d856081c8375509e2d096732c
Alexander Krupenkin <mail@akru.me>
2019-01-05 11:45:38 +0300

    profiles/aira-sidechain: fix params

--- nixos/modules/profiles/aira-sidechain.nix
+++ nixos/modules/profiles/aira-sidechain.nix
@@ -6,18 +6,19 @@ let
   lighthouse = "airalab.lighthouse.4.robonomics.sid";
   factory = "factory.4.robonomics.sid";
   graph_topic = "graph.4.robonomics.sid";
+  token = "xrt.4.robonomics.sid";
   ens = "0x4e978ed8A05b516D8130Ff7dF54Fbc8b7ceB6442";
 
 in {
   # Enable light robot liability service
   services.liability = {
     enable = true;
-    inherit ens web3_ws_provider web3_ws_provider lighthouse factory graph_topic;
+    inherit ens web3_http_provider web3_ws_provider lighthouse factory graph_topic;
   };
 
   # Set params for XRT
   services.erc20 = {
     enable = true;
-    inherit ens web3_ws_provider web3_ws_provider token factory;
+    inherit ens web3_http_provider web3_ws_provider token factory;
   };
 }
